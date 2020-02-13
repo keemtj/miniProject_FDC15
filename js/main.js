b0ab30a77b97c56c99e5f98bda032a5305191357
keemtj <keemgreat@gmail.com>
2020-02-13 10:28:19 +0900

    feat: Added main.js and nav.js
    - added hamburger button active effect for nav.js
    - added detail active effect for main.js
    
    todo: add closed detail active effect

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
-// miniProject
+// 메인
+const menuItem = document.querySelector('.main__menuItem');
+const menuDetail = document.querySelector('.main__menuDetail');
 
-var menuItems = $('.main__menuItem');
-var menuDetails = $('.main__menuDetail');
-var btnClosed = $('.menuDetail__btnClosed');
+const menuItems= document.querySelectorAll('.main__menuItem');
+const menuDetails = document.querySelectorAll('.main__menuDetail');
+const closeBtns = document.querySelectorAll('.menuDetail__btnClosed')
 
 
+for(let i = 0; i < menuItems.length; i++){
+    let items = menuItems[i];
+    items.addEventListener('click', detailUp);    
+}
 
-menuItems.click(function(){
-    menuDetails.show();
-});
+function detailUp(e) {
+    let _this = e.target;
+    let _parent = _this.closest('li');
+    let _one = _parent.querySelector('.main__menuDetail');
+    _one.classList.add('main__menuDetail--active');
+}
 
-btnClosed.click(function(){
-    menuDetails.hide();
-});
+for(let i = 0; i < closeBtns.length; i++){
+    let btns = closeBtns[i];
+    btns.addEventListener('click', btnClosed);
+}
+
+function btnClosed() {
+    // let _this = e.target;
+    // let _parent = _this.querySelectorAll('.main__menuDetail--active')
+    menuDetails.classList.remove('main__menuDetail--active');
+}
+
 
